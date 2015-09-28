cf93a21af0ee4d315ffab0b2d253dd3e3b654a7a
Jorge Durán <ganchito55@gmail.com>
2015-09-28 00:50:41 +0000

    Funciona el resaltador

--- Web Studio/MainWindow.xaml.cs
+++ Web Studio/MainWindow.xaml.cs
@@ -1,29 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using MahApps.Metro.Controls;
+﻿using System.Xml;
+using ICSharpCode.AvalonEdit.Highlighting;
+using ICSharpCode.AvalonEdit.Highlighting.Xshd;
 
 namespace Web_Studio
 {
     /// <summary>
     /// Lógica de interacción para MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : MetroWindow
+    public partial class MainWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+            XmlReader reader = XmlReader.Create("D:\\ProyectoCSharp\\otro.xshd");
+            textEditor.SyntaxHighlighting = HighlightingLoader.Load(reader, HighlightingManager.Instance);
+         
+
+
         }
     }
 }
