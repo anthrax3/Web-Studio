16b52c8758a8306a9bc2123928e204c0084d199d
Jorge Durán <ganchito55@gmail.com>
2015-10-07 01:08:13 +0000

    1º Binding de la configuracion

--- Web Studio/Options.xaml.cs
+++ Web Studio/Options.xaml.cs
@@ -1,39 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
+﻿using System.Globalization;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 using WPFLocalizeExtension.Engine;
 using WPFLocalizeExtension.Providers;
 
 namespace Web_Studio
 {
     /// <summary>
-    /// Lógica de interacción para Options.xaml
+    ///     Lógica de interacción para Options.xaml
     /// </summary>
     public partial class Options
     {
         public Options()
         {
             InitializeComponent();
+            this.DataContext = ConfigManager.Instance;
+            ComboBoxLanguage.ItemsSource = ResxLocalizationProvider.Instance.AvailableCultures;
+            //CultureInfo.CurrentUICulture.DisplayName
+            ComboBoxLanguage.DisplayMemberPath = "DisplayName";
         }
 
         private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
         {
             LocalizeDictionary.Instance.SetCurrentThreadCulture = true;
             LocalizeDictionary.Instance.Culture = new CultureInfo("es");
-           
         }
-
     }
 }
