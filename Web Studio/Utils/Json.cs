ab5a0cfe77a1b8ab5d401ecfaa3f17cb650d13a9
Jorge Durán <ganchito55@gmail.com>
2015-10-12 00:36:17 +0000

    Mejoras en clase auxiliar JSON

--- Web Studio/Utils/Json.cs
+++ Web Studio/Utils/Json.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.IO;
-using System.Windows.Controls;
 using Newtonsoft.Json;
 
 namespace Web_Studio.Utils
@@ -28,10 +27,33 @@ namespace Web_Studio.Utils
             }
             catch (Exception)
             {
-                return false;                
-                throw;
+                return false;       
+                
             }
             return true;
+        }
+
+        /// <summary>
+        /// Read an object from json file. In case of exception return null
+        /// </summary>
+        /// <param name="typeOfClass">It's needed to parse the json file</param>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public static object FileToObject(object typeOfClass,string path)
+        {
+            try
+            {
+                //Try to find the config file
+                using (StreamReader file = File.OpenText(path))
+                {
+                    JsonSerializer serializer = new JsonSerializer();
+                    return serializer.Deserialize(file, typeOfClass.GetType());
+                }
+            }
+            catch (Exception) //FileException or JSonException --> use default values
+            {
+                return null;
+            }
         }
     }
 
