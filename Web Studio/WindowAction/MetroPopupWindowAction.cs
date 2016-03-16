9c057436f5b4154db66a0071ae30b2f19bfc8532
Jorge Durán <ganchito55@gmail.com>
2016-03-16 23:15:57 +0100

    Crear las ventanas con Prism + mahapps

---
+++ Web Studio/WindowAction/MetroPopupWindowAction.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using Prism.Interactivity;
+using Web_Studio.Events;
+
+namespace Web_Studio.WindowAction
+{
+    /// <summary>
+    /// Override the default PopWindowAction to use a metro window (mahapps)
+    /// </summary>
+    public class MetroPopupWindowAction : PopupWindowAction
+    {
+        /// <summary>
+        /// it creates a new metro window instead of a window
+        /// </summary>
+        /// <returns></returns>
+        protected override Window CreateWindow()
+        {
+            return new MetroPopupWindow();
+
+        }
+    }
+}
