d35400429139355b30e41ae0d97b8bc8bef2657f
Jorge Durán <ganchito55@gmail.com>
2016-03-18 22:17:23 +0100

    TG-48 #done

---
+++ ValidationPlugins/Error404PagePlugin/Error404Page.cs
@@ -0,0 +1,140 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.IO;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using System.Windows.Controls;
+using Error404PagePlugin.Properties;
+using ValidationInterface;
+using ValidationInterface.CategoryTypes;
+using ValidationInterface.MessageTypes;
+
+namespace Error404PagePlugin
+{
+    /// <summary>
+    ///     Plugin to check and generate error 404 page
+    /// </summary>
+    [Export(typeof (IValidation))]
+    [ExportMetadata("Name", "Error404Page")]
+    [ExportMetadata("After", "Htaccess")]
+    public class Error404Page : IValidation
+    {
+        /// <summary>
+        /// </summary>
+        public Error404Page()
+        {
+            View = new View(this);
+        }
+
+        /// <summary>
+        ///     Text of AutoFix for binding
+        /// </summary>
+        public string AutoFixText => Strings.AutoFix; //Todo
+
+        #region IValidation
+
+        /// <summary>
+        ///     View showed when you select the plugin
+        /// </summary>
+        public UserControl View { get; }
+
+        /// <summary>
+        ///     Name of the plugin
+        /// </summary>
+        public string Name => Strings.Name;
+
+        /// <summary>
+        ///     Description
+        /// </summary>
+        public string Description => Strings.Description;
+
+        /// <summary>
+        ///     Category of the plugin
+        /// </summary>
+        public ICategoryType Type { get; } = DevelopmentType.Instance;
+
+        /// <summary>
+        ///     Results of the check method.
+        /// </summary>
+        public List<AnalysisResult> AnalysisResults { get; } = new List<AnalysisResult>();
+
+        /// <summary>
+        ///     can we automatically fix some errors?
+        /// </summary>
+        public bool IsAutoFixeable { get; set; } = false;
+
+        /// <summary>
+        ///     Is enabled this plugin
+        /// </summary>
+        public bool IsEnabled { get; set; } = true;
+
+        /// <summary>
+        ///     Method to validate the project with this plugin
+        /// </summary>
+        /// <param name="projectPath"></param>
+        /// <returns></returns>
+        public List<AnalysisResult> Check(string projectPath)
+        {
+            AnalysisResults.Clear();
+            if (!IsEnabled) return AnalysisResults;
+
+            var htaccessPath = Path.Combine(projectPath, ".htaccess");
+            if (!File.Exists(htaccessPath))
+            {
+                AnalysisResults.Add(new AnalysisResult
+                {
+                    PluginName = Name,
+                    File = "",
+                    Line = 0,
+                    Type = ErrorType.Instance,
+                    Message = Strings.NotFound
+                });
+            }
+            else
+            {
+                var content = File.ReadAllText(htaccessPath);
+                var match = Regex.Match(content, @"ErrorDocument 404 .*");
+                if (!match.Success)
+                {
+                    AnalysisResults.Add(new AnalysisResult
+                    {
+                        PluginName = Name,
+                        File = htaccessPath,
+                        Line = 0,
+                        Type = ErrorType.Instance,
+                        Message = Strings.TagNotFound
+                    });
+                }
+            }
+            return AnalysisResults;
+        }
+
+        /// <summary>
+        ///     Method to fix automatically some errors
+        /// </summary>
+        /// <param name="projectPath"></param>
+        public void Fix(string projectPath)
+        {
+            var configurationContent =
+                @"# ----------------------------------------------------------------------
+# | Custom error messages/pages                                        |
+# ----------------------------------------------------------------------
+
+# Customize what Apache returns to the client in case of an error.
+# https://httpd.apache.org/docs/current/mod/core.html#errordocument
+
+# ErrorDocument 404 /404.html";
+
+            if (!IsAutoFixeable) return;
+            var htaccessPath = Path.Combine(projectPath, ".htaccess");
+            File.AppendAllText(htaccessPath, configurationContent);
+
+            //Copy the 404 error page
+            var source = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "404.html");
+            var destination = Path.Combine(projectPath, "404.html");
+            File.Copy(source, destination);
+        }
+
+        #endregion
+    }
+}
