6a61c81d7c13cc2fc445f3c22a34c60d8f1b4290
Jorge Durán <ganchito55@gmail.com>
2016-03-19 13:48:27 +0100

    Sobrescrito el metodo de lanzar ventanas, ahora todas son metro

---
+++ Web Studio/WindowAction/MetroConfirmationWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Prism.Interactivity.InteractionRequest;
+
+namespace Web_Studio.WindowAction
+{
+    /// <summary>
+    /// Lógica de interacción para MetroConfirmationWindow.xaml
+    /// </summary>
+    public partial class MetroConfirmationWindow 
+    {
+        public MetroConfirmationWindow()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// Sets or gets the <see cref="IConfirmation"/> shown by this window./>
+        /// </summary>
+        public IConfirmation Confirmation
+        {
+            get
+            {
+                return this.DataContext as IConfirmation;
+            }
+            set
+            {
+                this.DataContext = value;
+            }
+        }
+
+        private void OkButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Confirmation.Confirmed = true;
+            this.Close();
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Confirmation.Confirmed = false;
+            this.Close();
+        }
+    }
+}
---
+++ Web Studio/WindowAction/MetroNotificationWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Prism.Interactivity.InteractionRequest;
+
+namespace Web_Studio.WindowAction
+{
+    /// <summary>
+    /// Lógica de interacción para MetroNotificationWindow.xaml
+    /// </summary>
+    public partial class MetroNotificationWindow 
+    {
+        public MetroNotificationWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Sets or gets the <see cref="INotification"/> shown by this window./>
+        /// </summary>
+        public INotification Notification
+        {
+            get
+            {
+                return this.DataContext as INotification;
+            }
+            set
+            {
+                this.DataContext = value;
+            }
+        }
+
+        private void OKButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
