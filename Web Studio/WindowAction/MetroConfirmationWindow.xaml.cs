9f6c6459124dc0814aabe4b382cb856bf303d9c9
Jorge Durán <ganchito55@gmail.com>
2016-03-19 14:11:22 +0100

    Añadir comentarios

--- Web Studio/WindowAction/MetroConfirmationWindow.xaml.cs
+++ Web Studio/WindowAction/MetroConfirmationWindow.xaml.cs
@@ -1,16 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
 using Prism.Interactivity.InteractionRequest;
 
 namespace Web_Studio.WindowAction
@@ -20,6 +8,9 @@ namespace Web_Studio.WindowAction
     /// </summary>
     public partial class MetroConfirmationWindow 
     {
+        /// <summary>
+        /// Default constructor
+        /// </summary>
         public MetroConfirmationWindow()
         {
             InitializeComponent();
