979adfcf1803b1a5eec3998c87224617fdc9720e
Jorge Durán <ganchito55@gmail.com>
2016-04-11 23:59:19 +0200

    TG-60 #done

---
+++ ValidationPlugins/NormalizeCssPlugin/View.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Controls;
+
+namespace NormalizeCssPlugin
+{
+    /// <summary>
+    ///     Lógica de interacción para View.xaml
+    /// </summary>
+    public partial class View : UserControl
+    {
+        /// <summary>
+        ///     Default constructor
+        /// </summary>
+        /// <param name="vm"></param>
+        public View(NormalizeCss vm)
+        {
+            InitializeComponent();
+            DataContext = vm;
+        }
+    }
+}
