5c5931424c416e08c10d1f1e2c6f629704d2f16f
Jorge Durán <ganchito55@gmail.com>
2016-04-12 21:00:09 +0200

    TG-65 #done

---
+++ ValidationPlugins/JoinAndMinifyJsPlugin/Stadistics.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+
+namespace JoinAndMinifyJsPlugin
+{
+    /// <summary>
+    ///     Minify Stadistics
+    /// </summary>
+    public class Stadistics
+    {
+        /// <summary>
+        ///     total size of all css files before minify
+        /// </summary>
+        public static double TotalJsSize = 0;
+
+        /// <summary>
+        ///     Compress ratio
+        /// </summary>
+        /// <param name="projectPath"></param>
+        /// <returns></returns>
+        public static double Ratio(string projectPath)
+        {
+            double minified = new FileInfo(Path.Combine(projectPath, "js", "script.js")).Length;
+            return minified/TotalJsSize;
+        }
+    }
+}
