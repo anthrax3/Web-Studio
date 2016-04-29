76449c0cb780c0550a4e6eac1d1b96c002e44665
Jorge Durán <ganchito55@gmail.com>
2016-04-29 19:30:53 +0200

    Añadir comentarios

--- Web Studio/Telemetry/Telemetry.cs
+++ Web Studio/Telemetry/Telemetry.cs
@@ -4,10 +4,19 @@ using Microsoft.ApplicationInsights;
 
 namespace Web_Studio.Telemetry
 {
+    /// <summary>
+    /// Manage Windows Telemetry in Azure
+    /// </summary>
     public static class Telemetry
     {
+        /// <summary>
+        /// client to send the telemetry data
+        /// </summary>
         public static readonly TelemetryClient TelemetryClient = new TelemetryClient();
 
+        /// <summary>
+        /// Telemetry config
+        /// </summary>
         public static void Initialize()
         {
             TelemetryClient.InstrumentationKey = "58f8f90f-bb85-4646-9d62-5009739028e5";
