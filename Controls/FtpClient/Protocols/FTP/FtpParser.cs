ef48b9f5471aa838ffe299adea6292101c111e0d
Jorge Durán <ganchito55@gmail.com>
2016-05-13 18:25:39 +0200

    TG-155 #closed

--- Controls/FtpClient/Protocols/FTP/FtpParser.cs
+++ Controls/FtpClient/Protocols/FTP/FtpParser.cs
@@ -21,7 +21,8 @@ namespace FtpClient.Protocols.FTP
             var items = new List<ProtocolItem>();
             foreach (var line in lines)
             {
-                items.Add(ItemParser(line, currentPath));
+                var result = ItemParser(line, currentPath);
+                if(result!=null) items.Add(result);
             }
             return items;
         }
@@ -54,7 +55,7 @@ namespace FtpClient.Protocols.FTP
             {
                 //Ignore. We have a problem parsing the FTP response
             }
-            return new ProtocolItem("aaa", "aaa", 0, DateTime.Today, FileType.Instance);
+            return null;
         }
 
         /// <summary>
@@ -108,9 +109,14 @@ namespace FtpClient.Protocols.FTP
             }
             var dayNumber = Convert.ToInt32(day);
             var parts = hourAndMinutes.Split(':');
-            var hoursNumber = Convert.ToInt32(parts[0]);
-            var minutesNumber = Convert.ToInt32(parts[1]);
-            return new DateTime(DateTime.Today.Year, monthNumber, dayNumber, hoursNumber, minutesNumber, 0);
+            if (parts.Length == 2) //We have Hours:Minutes example 12:55
+            {
+                var hoursNumber = Convert.ToInt32(parts[0]);
+                var minutesNumber = Convert.ToInt32(parts[1]);
+                return new DateTime(DateTime.Today.Year, monthNumber, dayNumber, hoursNumber, minutesNumber, 0);
+            }
+            //We have the Year
+            return new DateTime(hourAndMinutes[0],monthNumber,dayNumber,0,0,0);
         }
     }
 }
